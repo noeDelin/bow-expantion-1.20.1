4c4f752eed059f0ac64a403a29f8931b085a170a
Noé Delin <noe.delin.etu@univ-lille.fr>
2023-07-22 23:45:56 +0200

    first commit

---
+++ src/main/java/net/nonopaddle/bow_expantion/BowExpansionClient.java
@@ -0,0 +1,12 @@
+package net.nonopaddle.bow_expantion;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BowExpansionClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        
+    }
+    
+}
